ec96e67efe06dc1b0c8dffeb31ec276244349342
Rohan <rohanp10240@gmail.com>
2025-04-26 12:19:47 +0530

    Implemented the findKthFromEnd method to retrieve the k-th node from the end of a singly linked list

---
+++ KthNodeFromEnd/Main.java
@@ -0,0 +1,122 @@
+package KthNodeFromEnd;
+
+class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nLinked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
+    }
+
+    // WRITE HASLOOP METHOD HERE //
+    public Node findKthFromEnd(int k){
+        if (head == null) return null;
+        if (head==tail && k==1) return head;
+
+        Node temp1 = head;
+        Node temp2 = head;
+
+        for (int i=1; i<k; i++){
+            if (temp2.next == null) {
+                return null;
+            }
+            temp2 = temp2.next;
+        }
+
+        while (temp2.next != null) {
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        return temp1;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+        myLinkedList.append(4);
+        myLinkedList.append(5);
+        myLinkedList.append(6);
+        
+        myLinkedList.printAll();
+
+        System.out.println(myLinkedList.findKthFromEnd(2).value);
+        
+    }
+
+}
