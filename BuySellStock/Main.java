016e772793cc15fdc2d942f5c527948bd778a40d
Rohan <rohanp10240@gmail.com>
2025-05-05 17:21:13 +0530

    Updated the BuySellStock with optimized approch

--- BuySellStock/Main.java
+++ BuySellStock/Main.java
@@ -2,38 +2,20 @@ package BuySellStock;
 
 public class Main {
     static int profit(int[] prices){
+        int maxProfit = 0;
+        int minSoFar = prices[0];
 
-        int buyIndex = 0;
-        int buyStockAmount = prices[buyIndex];
-        int sellIndex = 0;
-        int sellStockAmount = prices[sellIndex];
-
-        for(int i=1; i<prices.length; i++){
-            if(prices[i]<buyStockAmount){
-                buyStockAmount = prices[i];
-                buyIndex = i;
-            }
+        for(int i=0; i<prices.length; i++){
+            minSoFar = Math.min(minSoFar, prices[i]);
+            int profit = prices[i] - minSoFar;
+            maxProfit = Math.max(maxProfit, profit);
         }
 
-        sellIndex = buyIndex; 
-        sellStockAmount = buyStockAmount;
-
-        for(int i=buyIndex+1; i<prices.length; i++){
-            if (prices[i]>sellStockAmount) {
-                sellStockAmount = prices[i];
-                sellIndex = i;
-            }
-        }
-
-        if(sellIndex>buyIndex){
-            return prices[sellIndex]-prices[buyIndex];
-        }
- 
-        return 0;
+        return maxProfit;
     }
     public static void main(String[] args) {
-        // int[] prices = {7,10,1,3,6,9,2};
-        int[] prices = {7,6,4,3,1};
+        int[] prices = {7,10,1,3,6,9,2};
+        // int[] prices = {7,6,4,3,1};
         System.out.println(profit(prices));
     }
 }
