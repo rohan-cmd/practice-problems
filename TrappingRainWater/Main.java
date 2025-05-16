cdc3a4cacb835964654db0c4cbc2ae37ce41f997
Rohan <rohanp10240@gmail.com>
2025-05-16 18:27:58 +0530

    Implemented a method to find the max water in container

--- TrappingRainWater/Main.java
+++ TrappingRainWater/Main.java
@@ -2,25 +2,20 @@ package TrappingRainWater;
 
 import java.util.*;
 
-// Function to return the maximum water that can be stored
 public class Main {
     static int maxWater(int[] arr) {
         int res = 0;
 
-        // For every element of the array
         for (int i = 1; i < arr.length - 1; i++) {
 
-            // Find the maximum element on its left
             int left = arr[i];
             for (int j = 0; j < i; j++)
                 left = Math.max(left, arr[j]);
 
-            // Find the maximum element on its right
             int right = arr[i];
             for (int j = i + 1; j < arr.length; j++)
                 right = Math.max(right, arr[j]);
 
-            // Update the maximum water
             res += Math.min(left, right) - arr[i];
         }
 
