cf4e8baf94da6272bdffec73888d7b44adb828e2
Rohan <rohanp10240@gmail.com>
2025-05-27 13:02:06 +0530

    Implemented a method to Binary to Decimal

---
+++ LinkedList/LL_BinarytoDecimal/Main.java
@@ -0,0 +1,121 @@
+package LinkedList.LL_BinarytoDecimal;
+
+class LinkedList {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public void printList() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+    
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    
+    public int binaryToDecimal(){
+        Node temp = head;
+        int num = 0;
+        while(temp != null){
+            num = num * 2 + temp.value;
+            temp = temp.next;
+        }
+        return num;
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // ---------------
+        // Convert 1011 to 11
+        // ---------------
+        LinkedList list1 = new LinkedList(1);
+        list1.append(0);
+        list1.append(1);
+        list1.append(1);
+        System.out.println("Convert 1011 to 11:");
+        System.out.println("Input: 1 -> 0 -> 1 -> 1");
+        System.out.println("Output: " + list1.binaryToDecimal());
+        System.out.println("---------------");
+
+        // ---------------
+        // Convert 1100 to 12
+        // ---------------
+        LinkedList list2 = new LinkedList(1);
+        list2.append(1);
+        list2.append(0);
+        list2.append(0);
+        System.out.println("Convert 1100 to 12:");
+        System.out.println("Input: 1 -> 1 -> 0 -> 0");
+        System.out.println("Output: " + list2.binaryToDecimal());
+        System.out.println("---------------");
+
+        // ---------------
+        // Convert 1 to 1
+        // ---------------
+        LinkedList list3 = new LinkedList(1);
+        System.out.println("Convert 1 to 1:");
+        System.out.println("Input: 1");
+        System.out.println("Output: " + list3.binaryToDecimal());
+        System.out.println("---------------");
+
+        // ---------------
+        // Convert empty list to 0
+        // ---------------
+        LinkedList list4 = new LinkedList(0);
+        list4.makeEmpty();
+        System.out.println("Convert empty list to 0:");
+        System.out.println("Input: empty");
+        System.out.println("Output: " + list4.binaryToDecimal());
+        System.out.println("---------------");
+    }
+    
+}
+
+
